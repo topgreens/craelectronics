9c8dca3b89c959706ac930630dd819cf3d68815b
topgreens <topgreens@mail.com>
2019-04-23 10:35:10 -0500

    pages modified

--- layout/scripts/toast.js
+++ layout/scripts/toast.js
@@ -1,41 +1,11 @@
 function Main() {
-    this.execute = function (){
-  // Get the modal
-var modal = document.getElementById('myModal');
+    this.execute = function msg(){
+        document.querySelector('#myBtn').onclick=delivered;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+      function delivered(){
+        alert('your message has been delivered')
+      }
+      window.onload= msg;
     }
   }
   new Main().execute();
-
-  //
-
-function msg(){
-  document.querySelector('btn').onclick= delivered;
-}
-function delivered(){
-  alert('you message has been delivered')
-}
-window.onload= msg;
-//
