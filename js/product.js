0038a428443a1269a2839ed13c6eeac88d521837
lorenzo876 <williamsramone59@gmail.com>
2019-04-24 20:10:39 +0200

    I remove the lorem ipsum

--- js/product.js
+++ js/product.js
@@ -2,7 +2,7 @@
 let product = {
    title: "HP Laptop",
    price: "€299.99",
-   description: "the best laptop in the market",
+   description: "The best laptop in the market,The all new modern ultra-slim design of the HP ProBook 645 delivers efficiently powerful performance",
    picture: "../images/pro_img/product_1.jpg"
 };
 
@@ -16,44 +16,44 @@ let product2 = {
 let product3 = {
    title: "Iphone X",
    price: "€1699.99",
-   description: "lorem lorewjbjsdibdkjsbdijsbdisbdibsdjs",
+   description: "Super Retina display in the iPhone X is the first high dynamic range (HDR) OLED display Apple incorporated into an iPhone.",
    picture: "../images/pro_img/product_3.jpg"
 };
 
 let product4 = {
    title: "Beats Headset",
    price: "€199.99",
-   description: "lorem lorewjbjsdibdkjsbdijsbdisbdibsdjs",
+   description: " Up to 40 hours of battery life, Beats Solo3 Wireless is your perfect everyday headphone",
    picture: "../images/pro_img/product_4.jpeg"
 };
 let product5 = {
    title: "Washing Machine",
    price: "€899.99",
-   description: "lorem lorewjbjsdibdkjsbdijsbdisbdibsdjs",
+   description: "It's unique tumble care technology senses the laundry conditions and washes the clothes in a unique 360° tumble motion.",
    picture: "../images/pro_img/product_5.jpg"
 };
 let product6 = {
    title: "beats Pill Speakers",
    price: "€279.99",
-   description: "lorem lorewjbjsdibdkjsbdijsbdisbdibsdjs",
+   description: " The Beats Pill+ speaker has up to 12 hours of battery life to stay charged on the go. Get a quick charge in 3 hours with the Lightning cable and",
    picture: "../images/pro_img/product_6.jpg"
 };
 
 
-let productObjects = [product, product2,product3,product4,product5,product6];
+let productObjects = [product, product2, product3, product4, product5, product6];
 for (let i = 0; i < productObjects.length; i++) {
    let currentProduct = productObjects[i];
    let product_Template = document.getElementById("product_template");
    let newProductElement = product_Template.cloneNode(true);
    delete newProductElement.id;
-   newProductElement.querySelector(".product-title").innerHTML =currentProduct.title;
+   newProductElement.querySelector(".product-title").innerHTML = currentProduct.title;
    newProductElement.querySelector(".title").innerHTML = currentProduct.title;
 
 
    newProductElement.querySelector(".price").innerHTML = currentProduct.price;
    newProductElement.querySelector(".description").innerHTML = currentProduct.description;
 
-   newProductElement.querySelector('.product-img').style.backgroundImage = "url(" +currentProduct.picture + ")";
+   newProductElement.querySelector('.product-img').style.backgroundImage = "url(" + currentProduct.picture + ")";
 
    let products = document.getElementById('products');
    products.appendChild(newProductElement);
